04fa5253d68372e787eabc45db3a6693bd08230a
Kehao Huang <huangk53@mcmaster.ca>
2021-01-20 23:16:31 -0500

    code.py update

--- lab1/code.py
+++ lab1/code.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+
+def are_valid_groups(snList, groupList):
+    counter = 0
+    for i in snList:
+        for j in range(0, len(groupList)):
+            for k in range(0, len(groupList[j])):
+                if i == groupList[j][k]:
+                    counter += 1
+    if counter >= len(snList):
+        return True
+    else:
+        return False
