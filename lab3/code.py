e36eb76e542031e7ed700335802087b5c7ea8d00
Anhao <jiaoa3@mcmaster.ca>
2021-02-04 00:05:54 -0500

    import basic sorts

--- lab3/code.py
+++ lab3/code.py
@@ -6,7 +6,8 @@ import numpy as np
 
 from lab3 import create_random_list, my_quicksort
 from sorts import (dual_pivot_quicksort, quad_pivot_quicksort,
-                   quicksort_inplace, tri_pivot_quicksort)
+                   quicksort_inplace, tri_pivot_quicksort,
+                   bubble_sort, insertion_sort, selection_sort)
 
 
 # in-place version timing experiments
@@ -62,44 +63,44 @@ def multp_exp():
 
 
 # Muti-pivot timing experiments
-# n_range = range(100_000, 1_000_000, 10_000)
-# quick_res = []  # my_quciksort results
-# dual_res = []   # dual_pivot_quicksort results
-# tri_res = []    # tri_pivot_quicksort results
-# qua_res = []    # quad_pivot_quicksort results
+n_range = range(100_000, 1_000_000, 10_000)
+quick_res = []  # my_quciksort results
+dual_res = []   # dual_pivot_quicksort results
+tri_res = []    # tri_pivot_quicksort results
+qua_res = []    # quad_pivot_quicksort results
 
-# for n in n_range:
-#     L = create_random_list(n)
-#     L2 = L.copy()
-#     L3 = L.copy()
-#     L4 = L.copy()
-#     qs1 = my_quicksort
-#     qs2 = dual_pivot_quicksort
-#     qs3 = tri_pivot_quicksort
-#     qs4 = quad_pivot_quicksort
-#     one_pivot = timeit.timeit("qs1(L)", globals=globals(), number=1)
-#     quick_res.append(one_pivot)
-#     two_pivot = timeit.timeit("qs2(L2)", globals=globals(), number=1)
-#     dual_res.append(two_pivot)
-#     three_pivot = timeit.timeit("qs3(L3)", globals=globals(), number=1)
-#     tri_res.append(three_pivot)
-#     four_pivot = timeit.timeit("qs4(L4)", globals=globals(), number=1)
-#     qua_res.append(four_pivot)
+for n in n_range:
+    L = create_random_list(n)
+    L2 = L.copy()
+    L3 = L.copy()
+    L4 = L.copy()
+    qs1 = my_quicksort
+    qs2 = dual_pivot_quicksort
+    qs3 = tri_pivot_quicksort
+    qs4 = quad_pivot_quicksort
+    one_pivot = timeit.timeit("qs1(L)", globals=globals(), number=1)
+    quick_res.append(one_pivot)
+    two_pivot = timeit.timeit("qs2(L2)", globals=globals(), number=1)
+    dual_res.append(two_pivot)
+    three_pivot = timeit.timeit("qs3(L3)", globals=globals(), number=1)
+    tri_res.append(three_pivot)
+    four_pivot = timeit.timeit("qs4(L4)", globals=globals(), number=1)
+    qua_res.append(four_pivot)
 
-# N = np.array(list(n_range))
-# qs1_data = np.array(quick_res)
-# qs2_data = np.array(dual_res)
-# qs3_data = np.array(tri_res)
-# qs4_data = np.array(qua_res)
+N = np.array(list(n_range))
+qs1_data = np.array(quick_res)
+qs2_data = np.array(dual_res)
+qs3_data = np.array(tri_res)
+qs4_data = np.array(qua_res)
 
-# # plotting for muti-poivt
-# plt.figure()
-# plt.plot(N, qs1_data, color='black', linewidth=0.5, label="my_quicksort")
-# plt.plot(N, qs2_data, color='green', linewidth=0.5, label="dual_pivot")
-# plt.plot(N, qs3_data, color='blue', linewidth=0.5, label="tri_pivot")
-# plt.plot(N, qs4_data, color='red', linewidth=0.5, label="quad_pivot")
-# plt.legend()
-# plt.show()
+# plotting for muti-poivt
+plt.figure()
+plt.plot(N, qs1_data, color='black', linewidth=0.5, label="my_quicksort")
+plt.plot(N, qs2_data, color='green', linewidth=0.5, label="dual_pivot")
+plt.plot(N, qs3_data, color='blue', linewidth=0.5, label="tri_pivot")
+plt.plot(N, qs4_data, color='red', linewidth=0.5, label="quad_pivot")
+plt.legend()
+plt.show()
 
 
 # Compare quicksort with elementary sorting methods
