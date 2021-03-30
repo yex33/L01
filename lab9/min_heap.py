9ae2bb555fe3d16909a4385adf11bae5fa276bbd
Xunzhou Ye <yex33@mcmaster.ca>
2021-03-30 19:18:59 -0400

    lab9 init

---
+++ lab9/min_heap.py
@@ -0,0 +1,226 @@
+import math
+
+class Element:
+
+    def __init__(self, value, key):
+        self.value = value
+        self.key = key
+
+    def __str__(self):
+        return "(" + str(self.value) + "," + str(self.key) + ")"
+
+    def __repr__(self):
+        return "Element(" + str(self.value) + "," + str(self.key) + ")"
+
+
+
+class MinHeap:
+
+    def __init__(self, L):
+        self.data = L
+        self.length = len(L)
+        self.map = {} # value -> index of that value
+        for i in range(len(self.data)):
+            self.map[self.data[i].value] = i
+        self.build_heap()
+
+
+    def build_heap(self):
+        for i in range(self.length // 2 - 1, -1, -1):
+            self.sink(i)
+
+    def sink(self, i):
+        smallest_known = i
+        if self.left(i) < self.length and self.data[self.left(i)].key < self.data[i].key:
+            smallest_known = self.left(i)
+        if self.right(i) < self.length and self.data[self.right(i)].key < self.data[smallest_known].key:
+            smallest_known = self.right(i)
+        if smallest_known != i:
+            self.data[i], self.data[smallest_known] = self.data[smallest_known], self.data[i]
+            self.map[self.data[i].value] = i
+            self.map[self.data[smallest_known].value] = smallest_known
+            self.sink(smallest_known)
+
+    def insert(self, element):
+        if len(self.data) == self.length:
+            self.data.append(element)
+        else:
+            self.data[self.length] = element
+        self.map[self.data[self.length].value] = self.length
+        self.length += 1
+        self.swim(self.length - 1)
+
+    def insert_elements(self, L):
+        for element in L:
+            self.insert(element)
+
+    def swim(self, i):
+        while i > 0 and self.data[i].key < self.data[self.parent(i)].key:
+            self.data[i], self.data[self.parent(i)] = self.data[self.parent(i)], self.data[i]
+            self.map[self.data[i].value] = i
+            self.map[self.data[self.parent(i)].value] = self.parent(i)
+            i = self.parent(i)
+
+    def get_min(self):
+        if len(self.data) > 0:
+            return self.data[0]
+
+    def extract_min(self):
+        self.data[0], self.data[self.length - 1] = self.data[self.length - 1], self.data[0]
+        min_element = self.data[self.length - 1]
+        self.length -= 1
+        self.sink(0)
+        return min_element
+
+    def decrease_key(self, value, new_key):
+        index = self.map[value]
+        self.data[index].key = new_key
+        self.swim(index)
+
+    def is_empty(self):
+        return self.length == 0
+
+    def left(self, i):
+        return 2 * (i + 1) - 1
+
+    def right(self, i):
+        return 2 * (i + 1)
+
+    def parent(self, i):
+        return (i + 1) // 2 - 1
+
+    def __str__(self):
+        height = math.ceil(math.log(self.length + 1, 2))
+        whitespace = 2 ** height
+        s = ""
+        for i in range(height):
+            for j in range(2 ** i - 1, min(2 ** (i + 1) - 1, self.length)):
+                s += " " * whitespace
+                s += str(self.data[j]) + " "
+            s += "\n"
+            whitespace = whitespace // 2
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
+
+L = []
+
+L.append(Element("A", 6))
+L.append(Element("B", 1))
+L.append(Element("C", 2))
+L.append(Element("D", 8))
+L.append(Element("E", 0))
+L.append(Element("G", 7))
+
+heap = MinHeap(L)
+
+
+"""
+class MinHeap:
+
+    def __init__(self, L):
+        self.data = L
+        self.length = len(L)
+        self.map = {}
+        for i in range(len(L)):
+            self.map[L[i].value] = i
+        self.build_heap()
+
+    def build_heap(self):
+        for i in range(self.length // 2 - 1, -1, -1):
+            self.sink(i)
+
+    def sink(self, i):
+        smallest_known = i
+        if self.left(i) < self.length and self.data[self.left(i)].key < self.data[i].key:
+            smallest_known = self.left(i)
+        if self.right(i) < self.length and self.data[self.right(i)].key < self.data[smallest_known].key:
+            smallest_known = self.right(i)
+        if smallest_known != i:
+            self.data[i], self.data[smallest_known] = self.data[smallest_known], self.data[i]
+            self.map[self.data[i].value] = i
+            self.map[self.data[smallest_known].value] = smallest_known
+            self.sink(smallest_known)
+
+    def insert(self, element):
+        if len(self.data) == self.length:
+            self.data.append(element)
+        else:
+            self.data[self.length] = element
+        self.map[element.value] = self.length
+        self.length += 1
+        self.swim(self.length - 1)
+
+    def insert_elements(self, L):
+        for element in L:
+            self.insert(element)
+
+    def swim(self, i):
+        while i > 0 and self.data[i].key < self.data[self.parent(i)].key:
+            self.data[i], self.data[self.parent(i)] = self.data[self.parent(i)], self.data[i]
+            self.map[self.data[i].value] = i
+            self.map[self.data[self.parent(i)].value] = self.parent(i)
+            i = self.parent(i)
+
+    def get_min(self):
+        if len(self.data) > 0:
+            return self.data[0]
+
+    def extract_min(self):
+        self.data[0], self.data[self.length - 1] = self.data[self.length - 1], self.data[0]
+        min_element = self.data[self.length - 1]
+        self.length -= 1
+        self.map.pop(min_element.value)
+        self.sink(0)
+        return min_element
+
+    def decrease_key(self, value, new_key):
+        if new_key >= self.data[self.map[value]].key:
+            return
+        index = self.map[value]
+        self.data[index].key = new_key
+        self.swim(index)
+
+    def get_element_from_value(self, value):
+        return self.data[self.map[value]]
+
+    def is_empty(self):
+        return self.length == 0
+
+    def left(self, i):
+        return 2 * (i + 1) - 1
+
+    def right(self, i):
+        return 2 * (i + 1)
+
+    def parent(self, i):
+        return (i + 1) // 2 - 1
+
+    def __str__(self):
+        height = math.ceil(math.log(self.length + 1, 2))
+        whitespace = 2 ** height
+        s = ""
+        for i in range(height):
+            for j in range(2 ** i - 1, min(2 ** (i + 1) - 1, self.length)):
+                s += " " * whitespace
+                s += str(self.data[j]) + " "
+            s += "\n"
+            whitespace = whitespace // 2
+        return s
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Element:
+
+    def __init__(self, value, key):
+        self.value = value
+        self.key = key
+
+    def __str__(self):
+        return "(" + str(self.value) + "," + str(self.key) + ")"
+"""
+
